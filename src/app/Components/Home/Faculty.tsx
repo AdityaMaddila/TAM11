3f89cb0993cb2e79847b2dc5298f208d5ee46dbf
S. Adithiya <adithiya1805@gmail.com>
2025-01-26 17:30:23 +0530

    Updated FA

--- src/app/Components/Home/Faculty.tsx
+++ src/app/Components/Home/Faculty.tsx
@@ -47,7 +47,7 @@ export default function StickyScrollRevealDemo() {
 
         <h3 className='sm:text-[18px] text-[14px] text-white tracking-wider font-bold text-center'> Group <span className='text-[#915eff]' >Director</span></h3>
         <h2 className='text-white font-bold md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px] text-center'>Dr.P.Santosh Kumar Patra</h2>
-        <p className= "sm:text-[18px] text-[14px] text-white tracking-wider ">Dr.P.Santosh Kumar Patra , Principal of St.Martin &apos; s Engineering College is holding B.E. in Computer Science & Engineering, MTech in Computer Science & Engineering and Ph.D in Computer Science & Engineering degrees. His area of interests are Airtificial Intelligence,Software Engineering , Data mining and warehousing & Wireless Networks.</p>
+        <p className= "sm:text-[18px] text-[14px] text-white tracking-wider ">Dr.P.Santosh Kumar Patra , Group Director of St.Martin &apos; s Engineering College is holding B.E. in Computer Science & Engineering, MTech in Computer Science & Engineering and Ph.D in Computer Science & Engineering degrees. His area of interests are Airtificial Intelligence,Software Engineering , Data mining and warehousing & Wireless Networks.</p>
         <br />
         <br />
         <h1 className='bg-clip-text text-transparent bg-gradient-to-b from-yellow-50 to-yellow-400 bg-opacity-50 font-bold md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px] text-center'>Faculty Advisor</h1>
@@ -58,7 +58,7 @@ export default function StickyScrollRevealDemo() {
       </div>
               <h3 className='sm:text-[18px] text-[14px] text-white tracking-wider font-bold text-center'>Faculty <span className='text-[#915eff]' >Advisor</span></h3>
         <h2 className='text-white font-bold md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px] text-center'>E. Soumya</h2>
-        <p className= "sm:text-[18px] text-[14px] text-white tracking-wider">E. Soumya , Faculty Advisor of TAM is holding B.E. in Computer Science & Engineering, MTech in Computer Science & Engineering and Ph.D in Computer Science & Engineering degrees. His area of interests are Airtificial Intelligence,Software Engineering , Data mining and warehousing & Wireless Networks.</p>
+        <p className= "sm:text-[18px] text-[14px] text-white tracking-wider">Every great organization has a great team and every great teams need a great Advisor. We as a crew at TAM have made it a point to ensure that every event is filled with a great deal of success, in addition to ensuring every participant is able to be served with equal priority. From planning the events to marketing, to making our website, we make sure that we are meeting the needs of every participant.</p>
         
       </motion.div>
       
